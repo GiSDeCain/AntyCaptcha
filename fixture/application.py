96c0b8ae6751affc3148137f730386c2d28eb88b
GiSDeCain <gisdecain8@gmail.com>
2019-03-08 19:01:54 +0100

    text negative created

--- fixture/application.py
+++ fixture/application.py
@@ -8,6 +8,7 @@ class Application:
     def __init__(self):
         self.driver = WebDriver()
         self.driver.implicitly_wait(5)
+        self.driver.get(Config.main_page)
         self.common = Common(self)
 
     def destroy(self):
--- tests/test_exercise_1.py
+++ tests/test_exercise_1.py
@@ -2,7 +2,6 @@ from config import Config
 
 
 def test_positive(fixture):
-    fixture.common.open_main_page()
     seed = fixture.common.get_seed()
     fixture.common.open_exercise(1, seed)
     fixture.common.click_button(1)
@@ -10,6 +9,12 @@ def test_positive(fixture):
     fixture.common.click_button(3)
     fixture.common.click_check_solution()
     assert fixture.common.check_trail() == Config.test_pass_text
+
+
+def test_negative(fixture):
+    fixture.common.open_exercise(1, test_positive(fixture).seed)
+    fixture.common.click_check_solution()
+    assert fixture.common.check_trail() == Config.test_fail_text
 
 
 __author__ = 'GiSDeCain'
