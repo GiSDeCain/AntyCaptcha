9e8a2e46ac78ec427141cf68ede7f2533efb5749
GiSDeCain <gisdecain8@gmail.com>
2019-03-13 13:26:54 +0100

    stf 3.2.1 works fine with logging and asertion in test, both positive and negative way

--- src/stf.py
+++ src/stf.py
@@ -13,12 +13,25 @@ class Stf:
 
     def open_stf_exercise(self, number, seed):
         driver = self.app.driver
+        wait = WebDriverWait(driver, 10)
+        sub_locator = Config.stf_url_sub_dir + str(number) + Config.ex_url_param + str(seed)
+        main_page_title = driver.find_element_by_xpath('//a[@href="/' + sub_locator + '"]').text
         url = Config.main_page + Config.stf_url_sub_dir + str(number) + Config.ex_url_param + str(seed)
         driver.get(url)
-        title = driver.find_element_by_link_text(Config.stf_url_sub_dir + str(number + Config.ex_url_param) + str(seed))
-        stf_title = driver.find_element_by_class_name('title').text
-        assert title.text in stf_title
+        stf_title = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'title')))
+        assert main_page_title.lower() in stf_title.text.lower()
         log.info('STF exercise ' + str(number) + ' opened successful')
+
+    def open_solution_url(self, seed):
+        driver = self.app.driver
+        url = Config.main_page + Config.stf_url_sub_dir + '3-2-1/solution' + Config.ex_url_param + str(seed)
+        driver.get(url)
+        log.info('Opened solution page using url: ' + url)
+
+    def stf_solution(self):
+        driver = self.app.driver
+        wait = WebDriverWait(driver, 10)
+        return wait.until(EC.presence_of_element_located((By.ID, 'trail'))).text
 
 
 __author__ = 'GiSDeCain'
