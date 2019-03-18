117c70d61934a73068e5473caec0f0b867f1a46f
GiSDeCain <gisdecain8@gmail.com>
2019-03-18 15:22:43 +0100

    minor refactor done.a little bit more refactor

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from fixture.application import Application
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def fixture(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
